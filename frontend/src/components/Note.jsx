3e193e8705de68a86f67c384bb89df55053f9afb
soroushb <sbahrami41@yahoo.com>
2024-04-16 20:27:56 -0400

    Logout created

--- frontend/src/components/Note.jsx
+++ frontend/src/components/Note.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import "../styles/Note.css"
+import api from "../api"
+import { useNavigate } from "react-router-dom"
+import {ACCESS_TOKEN, REFRESH_TOKEN} from "../constants"
+import "../styles/Form.css"
+import LoadingIndicator from "./LoadingIndicator"
 
 function Note({ note, onDelete }) {
     const formattedDate = new Date(note.created_at).toLocaleDateString("en-US")
 
+    const navigate = useNavigate()
+
     return (
+
+
+        <>
+        <div>
+            <button onClick={() => {
+                localStorage.removeItem(ACCESS_TOKEN);
+                localStorage.removeItem(REFRESH_TOKEN);
+                navigate("/login")}}>Logout</button>
+        </div>
         <div className="note-container">
             <p className="note-title">{note.title}</p>
             <p className="note-content">{note.content}</p>
@@ -13,6 +29,8 @@ function Note({ note, onDelete }) {
                 Delete
             </button>
         </div>
+        </>
+        
     );
 }
 
