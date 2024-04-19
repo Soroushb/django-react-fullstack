850d274b951d59754bfc729ad15daaf09aaeeffb
soroushb <sbahrami41@yahoo.com>
2024-04-19 11:25:15 -0400

    form error fixed

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -125,6 +125,7 @@ const Form = ({route, method}) => {
 
           <div className="relative mb-6" data-twe-input-wrapper-init>
             <input
+              value={password} onChange={(e) => setPassword(e.target.value)}
               type="password"
               className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[twe-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-white dark:placeholder:text-neutral-300 dark:autofill:shadow-autofill dark:peer-focus:text-primary [&:not([data-twe-input-placeholder-active])]:placeholder:opacity-0"
               id="exampleFormControlInput22"
